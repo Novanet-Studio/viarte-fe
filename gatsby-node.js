210db1e111601e3fa88caf610c780ac4b0e23266
Davejs136 <davejs136@gmail.com>
2020-09-16 13:20:37 -0400

    add: Blog page
    modify: route of templates
    
    remove: both blog files
    
    add: templates folder
    
    add: page blog.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,7 +27,7 @@ exports.createPages = async ({ actions:{ createPage }, graphql }) => {
 
     createPage({
       path: `/blog/${slug}`,
-      component: path.resolve('./src/pages/blog/entradas.js'),
+      component: path.resolve('./src/templates/entrada.js'),
       context: {
         id: node.id
       }
--- src/templates/entrada.js
+++ src/templates/entrada.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import Layout from '../../components/layout'
+import Layout from '../components/layout'
 import ReactMarkdown from 'react-markdown'
 
 const EntradaTemplate = ({ data }) => (
