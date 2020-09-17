c4c954777f147f2613944a684faec57e4438056b
Davejs136 <davejs136@gmail.com>
2020-09-17 15:06:14 -0400

    feat: articulo "Destacado"

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,6 +4,37 @@ import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const showArticles = (data) =>
+  data.allStrapiEntrada.edges.map(({ node }) =>
+    node.destacado ? (
+      <li key={node.id}>
+        <p style={{fontSize: '1rem', color: 'red'}}>DESTACADO</p>
+        <h2>
+          <Link to={`/blog/${node.Slug}`}>{node.title}</Link>
+          <Img
+            fluid={node.image.childImageSharp.fluid}
+            title={node.seo_image.title}
+            alt={node.seo_image.alt}
+          />
+        </h2>
+      </li>
+    ) : (
+      <li key={node.id}>
+        <p>NO DESTACADO: </p>
+        <h2>
+          <Link to={`/blog/${node.Slug}`}>{node.title}</Link>
+          <Img
+            fluid={node.image.childImageSharp.fluid}
+            title={node.seo_image.title}
+            alt={node.seo_image.alt}
+            width="200"
+            height="200"
+          />
+        </h2>
+      </li>
+    )
+  )
+
 const Blog = ({ data }) => (
   <Layout>
     <SEO
@@ -16,11 +47,12 @@ const Blog = ({ data }) => (
       title={data.strapiBlog.seo_image.title}
       alt={data.strapiBlog.seo_image.alt}
     >
-      <div className="contennedor">
+      <div className="contenedor">
         <h1>{data.strapiBlog.title}</h1>
         <p>{data.strapiBlog.description}</p>
         <ul>
-          {data.allStrapiEntrada.edges.map((item) => (
+          {showArticles(data)}
+          {/* {data.allStrapiEntrada.edges.map((item) => (
             <li key={item.node.id}>
               <h2>
                 <Link to={`/blog/${item.node.Slug}`}>{item.node.title}</Link>
@@ -32,7 +64,7 @@ const Blog = ({ data }) => (
               />
               <p>{item.node.description}</p>
             </li>
-          ))}
+          ))} */}
         </ul>
       </div>
     </div>
@@ -63,6 +95,7 @@ export const query = graphql`
           title
           description
           Slug
+          destacado
           image {
             childImageSharp {
               fluid(maxWidth: 450) {
