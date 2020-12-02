cfb59ca5589b1c41e9b2c17c9a8c2f187591f15d
robertonovanet <rhernandez@novanet.studio>
2020-12-02 12:05:56 -0430

    Maquetación de vistas Thank You y 404

---
+++ src/pages/thank-you.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "./thank-you.less"
+
+const ThankYouPage = () => (
+  <Layout>
+    <SEO title="Thank You" />
+    <div className="gracias">
+      <div className="contenedor">
+        <h2>Gracias, por enviar información</h2>
+        <p>
+          Sed beatae vut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque totam rem aperiam, eaque ipsa quae ab illo
+          inventore veritatis et quasi architecto beatae vitae dicta sunt
+          explicabo. Nemo enim
+        </p>
+        <Link to="/" className="gracias__boton">
+          Regresar al inicio
+        </Link>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default ThankYouPage
