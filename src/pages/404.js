74a64ace03426bf8251942da6c913611ca53e18e
robertonovanet <rhernandez@novanet.studio>
2020-12-02 12:05:56 -0430

    refactor: maquetacion de vistas thank-you y 404
    Maquetación de vistas Thank You y 404
    
    Maquetación de vistas Thank You y 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,24 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "./404.less"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="perdido">
+      <div className="contenedor">
+        <h2>No se ha encontrado la página</h2>
+        <p>
+          At vero eos et accusamus et iusto odio dign issimos ducimus qui
+          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+          et quas molestias excepturi sint occaecati cupiditate non provident,
+          similique sunt.
+        </p>
+        <Link to="/" className="perdido__boton">Regresar al inicio</Link>
+      </div>
+    </div>
   </Layout>
 )
 
