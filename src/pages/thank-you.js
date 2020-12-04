905b627516b097b36c047cfa875fe6b2f65fadbf
robertonovanet <rhernandez@novanet.studio>
2020-12-04 13:17:18 -0430

    Ajustes de diseño en página "404" y "Thank you"

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Gracias from "../assets/images/gracias-icono.svg"
 import "./thank-you.less"
 
 const ThankYouPage = () => (
@@ -9,12 +10,12 @@ const ThankYouPage = () => (
     <SEO title="Thank You" />
     <div className="gracias">
       <div className="contenedor">
-        <h2>Gracias, por enviar información</h2>
+        <h2>¡Gracias por enviarnos tu mensaje!</h2>
+        <img className="gracias__icono" src={Gracias} alt="icono gracias" />
         <p>
-          Sed beatae vut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque totam rem aperiam, eaque ipsa quae ab illo
-          inventore veritatis et quasi architecto beatae vitae dicta sunt
-          explicabo. Nemo enim
+          Te responderemos a la brevedad posible, si quieres saber más de
+          nosotros puedes hacer click en las redes sociales ubicadas en el pie
+          de la página.
         </p>
         <Link to="/" className="gracias__boton">
           Regresar al inicio
