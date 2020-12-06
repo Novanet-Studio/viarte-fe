db8a6e9d1ec70614fa932db655f2c552b26da095
Freddy Polanía <fpolania@gmail.com>
2020-12-06 13:31:24 -0400

    Cambios vistas 404 y thank-you

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Perdido from "../assets/images/404-icono.svg"
 import "./404.less"
 
 const NotFoundPage = () => (
@@ -9,14 +10,16 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <div className="perdido">
       <div className="contenedor">
+        <img className="perdido__icono" src={Perdido} alt="icono perdido" />
         <h2>No se ha encontrado la página</h2>
         <p>
-          At vero eos et accusamus et iusto odio dign issimos ducimus qui
-          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-          et quas molestias excepturi sint occaecati cupiditate non provident,
-          similique sunt.
+          La página que estás tratando de ver no existe, usa el menú de
+          navegación para ir a otra página o haz click en el botón para volver
+          al inicio.
         </p>
-        <Link to="/" className="perdido__boton">Regresar al inicio</Link>
+        <Link to="/" className="perdido__boton">
+          Regresar al inicio
+        </Link>
       </div>
     </div>
   </Layout>
